8afabb226109e3520c831b6fefaf781b36ab0779
Amit <kv_amit@yahoo.com>
2018-03-20 21:50:37 -0400

    expense tab

--- force-app/main/default/aura/ExpenseList/ExpenseListController.js
+++ force-app/main/default/aura/ExpenseList/ExpenseListController.js
@@ -3,14 +3,24 @@
         // Retrieve expenses during component initialization
         helper.loadexpenses(component);
     },
+    
     mysearch : function(component, event, helper) {
         alert('Hello1:- ' +  component.find("myinput").get("v.value"));
+        alert('Hello1:- ' +  event.getSource().getLocalId());
         var oreq = new XMLHttpRequest();
         oreq.addEventListener("load",function(){
             console.log(this.responseText);
         });
         alert('Hello2:- ' +  component.find("myinput").get("v.value"));
     },
+
+    changeDetected : function(component, event, helper) {
+        alert("changeDetected");
+        alert(event.target.id);
+        //alert (event.getSource().getLocalId());
+        //alert(whichOne);
+        //alert(cmp.getGlobalId());
+    }, 
 
     handleSelect : function(component, event, helper) {
         var expenses = component.get("v.expenses");
